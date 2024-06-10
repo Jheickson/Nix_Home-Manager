d4179af5910f2405dce122879a14da9417817ab2
Jheickson Felipe <felipesantosatm@outlook.com>
2024-06-10 16:46:46 -0300

    Flake Enable (didnt work)

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -92,6 +92,7 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   List packages installed in system profile. To search, run:
   # $ nix search wget
